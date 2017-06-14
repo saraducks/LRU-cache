398bd0f3e6a7b37d2f0693350efad0e8f227f774
saraducks <saranyadhakshina25@gmail.com>
2017-06-14 12:23:17 -0700

    added a node class

---
+++ src/node.py
@@ -0,0 +1,7 @@
+class Node:
+    #doubly linked list with previous and next pointer
+    def __init__(self, key, value):
+        key = key
+        value = value
+        self.previous = None
+        self.next = None
