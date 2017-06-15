3358d44bc0be83965fc6b6df1146ccfdf84f7e14
saraducks <saranyadhakshina25@gmail.com>
2017-06-15 12:56:59 -0700

    added removenode method

--- src/main.py
+++ src/main.py
@@ -13,6 +13,8 @@ class LRU:
     def addnode(self, key, value):
         #create node
         added_node = node.Node(key, value)
+        added_node.next = None
+        added_node.previous = None
         #check if node is created
         print("adding node")
         print(added_node)
@@ -20,19 +22,49 @@ class LRU:
         #check if the node head and tail is None
         if(self.head_node.Head == None):
             self.head_node.Head = added_node
+            self.head_node.Tail = added_node
             self.cachedata.first = added_node               #udpate the cahcedata head and tail
             self.cachedata.last = added_node
         #if the head is not none, then add the new node
         else:
-            self.head_node.previous = added_node
-            added_node.next = self.head_node
+            self.head_node.Head.previous = added_node
+            added_node.next = self.head_node.Head
             self.head_node.Head = added_node
             self.cachedata.first = added_node
+
+        return added_node
+
 
     '''
       Remove the least used node
     '''
-    #def removenode(self, node, key):
+    def removenode(self, remove_node):
+        #check if the head is not None
+        if(not self.head_node.Head == None):
+            # check if the node is head and head equals tail
+            if(self.head_node.Head == self.head_node.Tail):
+                print("I am executing.....")
+                self.head_node.Head = None
+                self.cachedata.first = None
+                self.cachedata.last = None
+            # check if the node is head and update the cache first
+            if(self.head_node.Head == remove_node):
+                self.head_node.Head.previous = None
+                self.head_node.Head = self.head_node.Head.next
+                self.cachedata.first = self.head_node.Head
+            # check if the node is tail and update the cache last
+            if(self.head_node.Tail == remove_node):
+                self.head_node.Tail = self.head_node.Tail.previous
+                self.head_node.Tail.next = None
+                self.cachedata.last = self.head_node.Tail
+            #if node is in the middle
+            else:
+                parent_node = remove_node.previous
+                decendant_node = remove_node.next
+                parent_node.next = decendant_node
+                decendant_node.previous = parent_node
+
+        return self.head_node.Head
 
 
 
@@ -41,8 +73,14 @@ class LRU:
 
 
 # create node and call addnode
-n = LRU()
-n.addnode('a',1)
-n.addnode('b',2)
-n.addnode('c',3)
-n.addnode('d',4)
+# n = LRU()
+# n.addnode('a',1)
+# n.addnode('b',2)
+# n.addnode('c',3)
+# n.addnode('d',4)
+r = LRU()
+r.addnode('x',10)
+res = r.addnode('y',20)
+r.addnode('z',30)
+r.removenode(res)
+
