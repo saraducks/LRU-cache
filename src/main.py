a1870b532547e09ce2eecf2e02cbbeec4542df2b
saraducks <saranyadhakshina25@gmail.com>
2017-06-15 23:59:35 -0700

    added cache data

--- src/main.py
+++ src/main.py
@@ -25,12 +25,14 @@ class LRU:
             self.head_node.Tail = added_node
             self.cachedata.first = added_node               #udpate the cahcedata head and tail
             self.cachedata.last = added_node
+            self.cachedata.hash[key] = added_node           #adding the node and key to hashtable
         #if the head is not none, then add the new node
         else:
             self.head_node.Head.previous = added_node
             added_node.next = self.head_node.Head
             self.head_node.Head = added_node
             self.cachedata.first = added_node
+            self.cachedata.hash[key] = added_node             #adding the node and key to hashtable
 
         return added_node
 
@@ -47,27 +49,46 @@ class LRU:
                 self.head_node.Head = None
                 self.cachedata.first = None
                 self.cachedata.last = None
+                self.cachedata.hash.__delitem__(remove_node.key)
+
             # check if the node is head and update the cache first
             if(self.head_node.Head == remove_node):
                 self.head_node.Head.previous = None
                 self.head_node.Head = self.head_node.Head.next
                 self.cachedata.first = self.head_node.Head
+                self.cachedata.hash.__delitem__(remove_node.key)
+
             # check if the node is tail and update the cache last
             if(self.head_node.Tail == remove_node):
                 self.head_node.Tail = self.head_node.Tail.previous
                 self.head_node.Tail.next = None
                 self.cachedata.last = self.head_node.Tail
+                self.cachedata.hash.__delitem__(remove_node.key)
+
             #if node is in the middle
             else:
                 parent_node = remove_node.previous
                 decendant_node = remove_node.next
                 parent_node.next = decendant_node
                 decendant_node.previous = parent_node
+                self.cachedata.hash.__delitem__(remove_node.key)
 
         return self.head_node.Head
 
+    '''
+      get method to check if the key is already present move to the head
+    '''
+    def __get__(self, key):
+        # check if the key is already present in hashtable
+        if(self.cachedata.hash.__contains__(key)):
+            move_to_head = self.cachedata.hash.get(key)      #get the node
+            #remove the node from the linkedlist
+            #add the removed node to the head
 
 
+    '''
+      set method to verify if the cache capacity reached it's limit and delete the elements
+    '''
 
 
 
@@ -82,5 +103,6 @@ r = LRU()
 r.addnode('x',10)
 res = r.addnode('y',20)
 r.addnode('z',30)
+print(res.key)
 r.removenode(res)
 
